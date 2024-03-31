d73fc9863d35296adee297c36cab1207b49e2c7d
Anurag Tiwari <120814417+anurag-tiwari-up@users.noreply.github.com>
2024-03-31 19:22:07 +0530

    abc

--- script.js
+++ script.js
@@ -7,6 +7,33 @@ var like_show = document.querySelector("#like_number span")
 var indicator=0
 var like_count=268
 
+
+like_indicator.addEventListener("click", function(){
+    like.style.transform="translate(-50%, -90%) scale(1)"
+    setTimeout(function(){
+        like.style.transform="translate(-50%, -90%) scale(0)"
+    },400);
+    setTimeout(function(){
+        like.style.opacity="0"
+    },450);
+    like.style.opacity=".8"
+
+    if(indicator==0){
+        like_indicator.style.color="red"
+        indicator+=1;
+        like_count++
+        like_show.innerHTML=like_count
+    }
+    else{
+        like_indicator.style.color="pink"
+        indicator-=1;
+        like_count--;
+        like_show.innerHTML=like_count
+    }
+})
+
+
+
 action.addEventListener("dblclick", function(){
     like.style.transform="translate(-50%, -90%) scale(1)"
     setTimeout(function(){
